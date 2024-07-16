283b8be7d4fe2689c14e6c6c983450b97b51e633
adrianaeif <adrianaisea02@gmail.com>
2024-07-16 01:20:07 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,7 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon", "especies"];
+  let ext = [".com", ".net", ".us", ".io", ".es"];
 
-  
+  pronoun.forEach(pronombre => {
+    adj.forEach(adjetivo => {
+      noun.forEach(nombre => {
+        ext.forEach(extension => {
+          const ultimasNLetras = nombre.slice(-1 * (extension.length - 1));
+          const extensionSimple = extension.slice(1);
+
+          if (ultimasNLetras == extensionSimple) {
+            console.log(
+              pronombre +
+                adjetivo +
+                nombre.slice(0, nombre.length - ultimasNLetras.length) +
+                extension
+            );
+          } else {
+            console.log(pronombre + adjetivo + nombre + extension);
+          }
+        });
+      });
+    });
+  });
+
   console.log("Hello Rigo from the console!");
 };
--- src/domain.js
+++ src/domain.js
@@ -1,15 +1,10 @@
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
-let ext = [".com",".net", ".us", ".io", ".es"];
+// const nombre = "arboles";
+// const extension = ".es";
+// const ultimasNLetras = nombre.slice(-1 * (extension.length - 1));
+// const extensionSimple = extension.slice(1);
 
-pronoun.forEach((pronombre) => {
-  adj.forEach((adjetivo) => {
-    noun.forEach((nombre) => {
-      ext.forEach((extension) => {
-        console.log(pronombre + adjetivo + nombre + extension);
-
-      });
-    });
-  });
-});
+// if (nombre.endsWith(extensionSimple)) {
+//   console.log(
+//     nombre.slice(0, nombre.length - ultimasNLetras.length) + extension
+//   );
+// }
